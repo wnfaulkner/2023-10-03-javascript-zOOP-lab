d6cefd3bd45479f3bb3032baeb70ab7471ea24cb
wnf <william@fluxrme.com>
2023-10-03 05:37:34 -0500

    finishes extending animal to mammal subclass

--- index.js
+++ index.js
@@ -27,8 +27,42 @@ class Animal {
 const tony = new Animal('tony','tiger','5','best boy')
 
 //Tests
-console.log(tony)
-tony.describe()
-tony.feed()
-tony.makeSound()
-tony.move()
+    // console.log(tony)
+    // tony.describe()
+    // tony.feed()
+    // tony.makeSound()
+    // tony.move()
+
+//Extending to a subclass
+class Mammal extends Animal {
+    constructor(name, species, age, gender, furColor){
+        super(name, species, age, gender)
+        this.furColor = furColor
+    }
+
+    describe(){
+        console.log(`This ${this.species} is named ${this.name} and has ${this.furColor} fur. They are a ${this.age} year-old ${this.gender}!`)
+    }
+
+    feed(){
+        console.log(`${this.name} is drinking milk.`)
+    }
+
+    makeSound(){
+        console.log(`${this.name} is howling.`)
+    }
+
+    move(){
+        console.log(`${this.name} is walking.`)
+    }   
+
+}
+
+//Instantiate
+    const louis = new Mammal('king louis','orangutang','12','king of the jungle','orange')
+
+//Tests
+    // louis.describe()
+    // louis.feed()
+    // louis.makeSound()
+    // louis.move()
